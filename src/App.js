8e28aeb92d6d4bcdb23bade3d6bc44f032f9a91a
alv18051 <alv18051@uvg.edu.gt>
2023-11-15 14:24:28 -0600

    boku no kotowo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg'; // Replace with your logo path
+
+const App = () => {
+  return (
+    <div>
+      <img src={logo} alt="Logo" style={{ width: '50px', height: '50px' }} />
+      <h1>Publica</h1>
+    </div>
+  );
+};
+
+export default App;
+
